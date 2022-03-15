b69248b0980babef32c34416513ea36502637acf
Andrew Walters <andrewjpwalters@gmail.com>
2022-03-14 22:00:59 -0500

    commit tree 3c0b832868202595057ed40b522bb7eb2e95f951

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Louise';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
